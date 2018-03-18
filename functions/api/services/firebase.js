9d0ffcc4a6787167b3b915c87347db9fe760a0f8
Minh <hlminh2000@gmail.com>
2018-03-18 11:30:01 -0400

    fixes deployment

--- functions/api/services/firebase.js
+++ functions/api/services/firebase.js
@@ -1,9 +1,12 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("../serviceAccount.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
-});
+admin.initializeApp(
+  {
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+  },
+  "api"
+);
 
-module.exports = admin
+module.exports = admin;
