a0dc515ab52d8ffd67986269827d8bd47d9397c9
Minh <hlminh2000@gmail.com>
2018-03-23 20:35:12 -0400

    implements puts

--- functions/api/resolvers/mutation/index.js
+++ functions/api/resolvers/mutation/index.js
@@ -2,6 +2,7 @@ const types = require("../types/index.js");
 const verifyIdToken = require("../../services/auth.js").verifyIdToken;
 const getPathSnapshotValue = require("../../services/database.js")
   .getPathSnapshotValue;
+const putValueToPath = require("../../services/database.js").putValueToPath;
 
 module.exports = {
   something: (_, { idToken }) => {
@@ -10,13 +11,23 @@ module.exports = {
   updateUserActivities: (_, { idToken, activityData } = {}) =>
     new Promise((resolve, reject) =>
       verifyIdToken(idToken)
-        .then(() => resolve([]))
+        .then(({ uid }) =>
+          putValueToPath(`users/${uid}/activities`, activityData)
+            .then(() => getPathSnapshotValue(`users/${uid}/activities`))
+            .then(activities => resolve(activities))
+        )
         .catch(err => resolve(null))
     ),
   updateUserActivityLogs: (_, { idToken, activityLogs, date } = {}) =>
     new Promise((resolve, reject) =>
       verifyIdToken(idToken)
-        .then(() => resolve([]))
+        .then(({ uid }) =>
+          putValueToPath(`users/${uid}/activityLogs/${date}`, activityLogs)
+            .then(() =>
+              getPathSnapshotValue(`users/${uid}/activityLogs/${date}`)
+            )
+            .then(activityLogs => resolve(activityLogs))
+        )
         .catch(err => resolve(null))
     )
 };
